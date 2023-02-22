50c5ee88197041baebb7f1051accdc6da97588c1
BenjiWarburton <114415469+BenjiWarburton@users.noreply.github.com>
2023-02-22 12:24:30 +0000

    22/02/2022

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import imageTaker
 from exif import Image
 import csv
 import reverse_geocoder as rg
@@ -18,7 +17,6 @@ base_folder = Path(__file__).parent.resolve()
 
 #takes the images and stores them
 def takeImages():
-
     camera = PiCamera()
     camera.resolution = (2592, 1944)
 
@@ -27,28 +25,34 @@ def takeImages():
     i = 0
     
     array = []
-    
-    while (timeNow < startTime + timedelta(minutes=1)):
+    while (timeNow < startTime + timedelta(seconds=10)):
         path = f'{base_folder}/images/image_{i:03d}.jpg'
-        t = timescale.now()
         #waits until the iss is in sunlight
-        while ISS.at(t).is_sunlit(ephemeris) == False:
-            t = timescale.now()
+        t = timescale.now()
+        #while ISS.at(t).is_sunlit(ephemeris) == False:
+            #t = timescale.now()
         point = ISS.coordinates()
         coordinates = (point.latitude.degrees, point.longitude.degrees)
+        country = coordinateToCountry(coordinates)
         camera.capture(path)
-        NVDIVal = calculateNVDI(path)
+        NDVIpath = f'{base_folder}/ndviImages/NDVIimage_{i:03d}.jpg'
+        NVDIVal = calculateNVDI(path, NDVIpath)
         #adds the data to an array to save into a csv later
-        array.append([NVDIVal, coordinates, path])
+        array.append([NVDIVal, coordinates, path, country])
         i = i + 1
-        sleep(3)
+        sleep(0.5)
+        timeNow = datetime.now()
     return array
 
 #measuring plant health
 def increaseContrast(path):
     OGImageim = cv2.imread(path) #read the image
     OGImage = np.array(OGImageim, dtype=float)/float(255) #convert into an array so maths can work
-
+    shape = OGImage.shape
+    height = int(shape[0] / 2)
+    width = int(shape[1] / 2)
+    OGImage = cv2.resize(OGImage, (width, height))
+    
     inMinC = np.percentile(OGImage, 5) #min brightness in the image
     inMaxC = np.percentile(OGImage, 95) #max brightness in the image
 
@@ -61,33 +65,29 @@ def increaseContrast(path):
 
     return outputImage
 
-def calculateNVDI(path):
+def calculateNVDI(path, NDVIpath):
     image = increaseContrast(path)
     b, _, r = cv2.split(image)
     bottom = (r.astype(float) + b.astype(float))
     bottom[bottom==0] = 0.01
     ndvi = (b.astype(float) - r) / bottom
-    return ndvi
+    cv2.imwrite(NDVIpath, ndvi)
+    return NDVIpath
 
 #converts coordinates to the name of the country the coordinates correspond to
 def coordinateToCountry(coordinates):
     results = rg.search(coordinates)
-    country = results.cc
+    country = results[0]['cc']
     return country
 
 #argument is a 2d Array
 def convertToCSV(array):
-    with open('data.csv', 'wb') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(['NDVI', 'image path', 'raw coordinate', 'country'])
+    with open('data.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile)
+        filewriter.writerow(['NDVI', 'raw-coordinate', 'image-path', 'country'])
         for i in range(0, len(array)):
-            country, city = coordinateToCountry(array[i][1])
-            filewriter.writerow([array[i][0], array[i][1], array[i][2], country])
+            filewriter.writerow([array[i][0], array[i][1], array[i][2], array[i][3]])
 
-array = imageTaker.takeImages() #takes images for the duration of the flight 
+array = takeImages()
+print("images taken")#takes images for the duration of the flight 
 convertToCSV(array)
-
-
-
-
